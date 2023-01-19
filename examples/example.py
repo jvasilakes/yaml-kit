b8b550bd52cd479481695dfc71ef8d7946038bb6
Jake Vasilakes <jvasilakes@gmail.com>
2023-01-19 17:42:35 +0000

    Add command line functionality.

--- examples/example.py
+++ examples/example.py
@@ -1,7 +1,4 @@
-import os
-import argparse
-
-from experiment_config import Config
+from experiment_config import Config, get_and_run_config_command
 
 
 config = Config("ExampleConfig")
@@ -14,7 +11,7 @@ def loss_fn(val):
 
 @config.parameter(types=str)
 def metric(val):
-    pass
+    assert val in ["accuracy", "f1"]
 
 
 @config.parameter(group="Training", default=2, types=int)
@@ -50,7 +47,7 @@ def hidden_dim(val):
 
 
 # It's okay to overload a parameter name if they belong to different groups.
-@config.parameter(group="Model.Decoder", default=5, types=int)
+@config.parameter(group="Model.Decoder", default=6, types=int)
 def input_dim(val):
     """
     Must be the same as Model.Encoder.hidden_dim
@@ -70,57 +67,5 @@ def validate_parameters():
     assert config.Model.Encoder.hidden_dim == config.Model.Decoder.input_dim
 
 
-# The following is an example of how you can create a command
-# line tool to work with config files.
-def update_config(filepath, **updates):
-    config.load_yaml(filepath)
-    for (key, value) in updates.items():
-        tmp = key.split('.')
-        group = '.'.join(tmp[:-1])
-        param = tmp[-1]
-        config.update(param, value, group=group)
-    os.rename(filepath, f"{filepath}.orig")
-    config.yaml(filepath)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="command")
-
-    print_parser = subparsers.add_parser(
-        "print", help="Print a config file to the terminal.")
-    print_parser.add_argument("filepath", type=str,
-                              help="The yaml file to print.")
-
-    newconf_parser = subparsers.add_parser(
-        "new", help="Save a new default config file.")
-    newconf_parser.add_argument("filepath", type=str,
-                                help="Where to save the new config file.")
-
-    update_parser = subparsers.add_parser(
-        "update", help="Update one or more config files with new parameter values.")  # noqa
-    update_parser.add_argument("-p", "--param", nargs=2, action="append",
-                               help="E.g., -p Model.Encoder.input_dim 2")
-    update_parser.add_argument("-f", "--files", nargs='+', type=str,
-                               help="Config files to update.")
-
-    return parser.parse_args()
-
-
 if __name__ == "__main__":
-    args = parse_args()
-
-    if args.command == "print":
-        config.load_yaml(args.filepath)
-        print(config)
-
-    elif args.command == "new":
-        config.yaml(args.filepath)
-
-    elif args.command == "update":
-        if args.param is None:
-            update_params = {}
-        else:
-            update_params = dict(args.param)
-        for filepath in args.files:
-            update_config(filepath, **update_params)
+    get_and_run_config_command(config)
---
+++ experiment_config/command.py
@@ -0,0 +1,93 @@
+import os
+import argparse
+
+from ruamel.yaml import YAML
+
+
+def update_config(config, filepath, **updates):
+    """
+    Load the config with parameters from a yaml file at filepath.
+    Update parameters using the key, value pairs in updates.
+
+    Keys in updates use the dot syntax to specify groups, subgroups,
+    and parameters. E.g., 'Group.Subgroup.parameter'.
+    """
+    # TODO: This will ignore errors that aren't set during the updates
+    # so some non-updated parameters might still be incorrect.
+    config.load_yaml(filepath, errors="ignore")
+    for (key, value) in updates.items():
+        tmp = key.split('.')
+        group = '.'.join(tmp[:-1])
+        param = tmp[-1]
+        config.update(param, value, group=group)
+    os.rename(filepath, f"{filepath}.orig")
+    config.yaml(filepath)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
+
+    print_parser = subparsers.add_parser(
+        "print", help="Print a config file to the terminal.")
+    print_parser.add_argument("filepath", type=str,
+                              help="The yaml file to print.")
+
+    val_parser = subparsers.add_parser(
+        "validate", help="Validate a config file.")
+    val_parser.add_argument("filepath", type=str,
+                            help="The yaml file to validate.")
+
+    newconf_parser = subparsers.add_parser(
+        "new", help="Save a new default config file.")
+    newconf_parser.add_argument("filepath", type=str,
+                                help="Where to save the new config file.")
+
+    update_parser = subparsers.add_parser(
+        "update", help="Update one or more config files with new parameter values.")  # noqa
+    update_parser.add_argument("-p", "--param", nargs=2, action="append",
+                               help="E.g., -p Model.Encoder.input_dim 2")
+    update_parser.add_argument("-f", "--files", nargs='+', type=str,
+                               help="Config files to update.")
+
+    return parser.parse_args()
+
+
+def run_command(config, args):
+    if args.command == "print":
+        config.load_yaml(args.filepath)
+        print(config)
+
+    elif args.command == "validate":
+        config.load_yaml(args.filepath, errors="warn")
+
+    elif args.command == "new":
+        config.yaml(args.filepath)
+
+    elif args.command == "update":
+        if args.param is None:
+            update_params = {}
+        else:
+            update_params = dict(args.param)
+        for filepath in args.files:
+            update_config(config, filepath, **update_params)
+
+
+def get_and_run_config_command(config):
+    """
+    Given a Config instance, get the command line arguments
+    passed to this script and run them. Add the following to
+    the end of your config definition file to expose the
+    command line interface.
+
+    .. code-block:: python
+
+        myconfig = Config("MyConfig")
+        @myconfig.parameter()
+        # etc.
+
+        if __name__ == "__main__":
+            get_and_run_config_command(myconfig)
+    """
+    args = parse_args()
+    run_command(config, args)
