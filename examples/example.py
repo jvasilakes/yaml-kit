254a975b4c1501863636355fe6a6799c9e467475
jvasilakes <jvasilakes@sift.net>
2026-04-22 14:01:14 -0500

    Refactor examples

--- examples/example.py
+++ examples/example.py
@@ -1,79 +1,45 @@
 from yaml_kit import Config, get_and_run_config_command
 
+config = Config("MyConfig")
 
-config = Config("ExampleConfig")
 
-
-@config.parameter(types=str, default="Example")
+@config.parameter(group="Experiment", types=str)
 def name(val):
-    pass
+    # Code to validate or modify `val` or `pass` to do nothing.
+    assert len(val) > 0
 
 
-@config.parameter(types=str)
-def metric(val):
-    assert val in ["accuracy", "f1"]
-
-
-@config.parameter(group="Losses")
-def loss_fn(val):
-    assert val in ["cross-entropy", "mse"]
-
-
-@config.parameter(group="Training", default=2, types=int)
-def batch_size(val):
-    """
-    Docstrings will be saved into the config and printed
-    as comments in the yaml file.
-    """
-    assert val > 0
-
-
-@config.parameter(group="Training", default=False, deprecated=True)
-def use_old_method(val):
-    pass
-
-
-@config.parameter(group="Model", default=0.0, types=float)
-def dropout_prob(val):
-    assert 0.0 <= val <= 1.0
-
-
-@config.parameter(group="Model.Encoder", default=5, types=int)
+@config.parameter(group="Model.Encoder", types=int, default=10)
 def input_dim(val):
     assert val > 0
 
 
-@config.parameter(group="Model.Encoder", default=5, types=int)
-def hidden_dim(val):
-    """
-    Must be the same as Model.Decoder.input_dim
-    """
-    assert val > 0
-
-
-# It's okay to overload a parameter name if they belong to different groups.
-@config.parameter(group="Model.Decoder", default=6, types=int)
-def input_dim(val):
-    """
-    Must be the same as Model.Encoder.hidden_dim
-    """
-    assert val > 0
-
-
-@config.parameter(group="Model.Decoder", default=5, types=int)
+@config.parameter(group="Model.Encoder", types=int, default=5)
 def output_dim(val):
     assert val > 0
 
 
-# The on_load decorator can be used to set restrictions
-# on combinations of parameters.
-@config.on_load
-def validate_parameters():
-    assert config.Model.Encoder.hidden_dim == config.Model.Decoder.input_dim
+@config.parameter(group="Model.Decoder", types=int, default=5)  # noqa
+def input_dim(val):
+    assert val > 0
+
+
+@config.parameter(group="Model.Decoder", types=int, default=3)  # noqa
+def output_dim(val):
+    assert val > 0
+
+
+@config.parameter(group="Model.Decoder", types=list, default=[5, 3], deprecated=True)
+def shape(val):
+    assert len(val) == 2
+    for member in val:
+        assert isinstance(member, int)
+        assert member > 0
+
 
 @config.on_load
-def modify_parameter():
-    config.Model.dropout_prob.value = 0.5
+def validate_model_shapes():
+    assert config.Model.Encoder.output_dim == config.Model.Decoder.input_dim
 
 
 if __name__ == "__main__":
